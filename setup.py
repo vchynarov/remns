4fbbb59d48ccae7e3e1d6beca023c0508195cf9a
Viktor Chynarov <viktor.chynarov@gmail.com>
2015-03-15 21:57:21 -0500

    seems to be working for now

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0.dev1',
+    version='0.1.0',
 
     description='An elegant Python blogging engine.',
     long_description='An elegant Python blogging engine based on Werkzeug and SQLAlchemy',
