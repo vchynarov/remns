c5767f775a23ef8bce25c14dd1e87ef15635b944
Viktor Chynarov <viktor.chynarov@gmail.com>
2015-03-21 20:29:37 -0500

    improved gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,31 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
+    path = require('path'),
     uglify = require('gulp-uglify');
+
 
 var build_tasks = [
   'build-epiceditor',
   'build-vendor-css',
   'build-vendor-js',
-  'build-admin-js'
+  'build-admin-js',
+  'move-templates'
 ];
 
-var distdir = 'dist/static';
+var DISTDIR = 'remns/dist';
+var dist = {
+    'static': path.join(DISTDIR, 'static', 'admin'),
+    'templates': path.join(DISTDIR, 'templates', 'admin'),
+    'epiceditor': path.join(DISTDIR, 'static', 'admin', 'epiceditor')
+};
 
 gulp.task('default', build_tasks);
 
 gulp.task('watch', function() {
-    gulp.watch(remnsPaths, ['build-admin-js']);
+    gulp.watch(remnsPaths.js.concat(remnsPaths.templates), build_tasks);
 });
  
-var adminPaths = {
+var bowerPaths = {
     'css' : [
         'bower_components/semantic-ui/dist/semantic.min.css',
      ],
@@ -25,37 +33,48 @@ var adminPaths = {
         'bower_components/jquery/dist/jquery.min.js',
         'bower_components/epiceditor/epiceditor/js/epiceditor.js',
         'bower_components/semantic-ui/dist/semantic.js'
-     ],
+     ]
 };
 
-var remnsPaths = [
-    'static/js/*.js'
-];
+var remnsPaths = {
+    'js' : [
+        'static/js/*.js',
+    ],
+    'templates' : [
+        'static/templates/*.html'
+    ]
+};
+
+gulp.task('move-templates', function() {
+    return gulp.src(remnsPaths.templates)
+        .pipe(gulp.dest(dist.templates));
+});
 
 gulp.task('build-admin-js', function() {
-    return gulp.src(remnsPaths)
+    return gulp.src(remnsPaths.js)
         .pipe(concat('utils.js'))
         .pipe(uglify())
         .on('error', function(e) {
             console.log('Invalid JS file format.');
         })
-        .pipe(gulp.dest(distdir));
+        .pipe(gulp.dest(dist.static));
 
 });
+//
 // epiceditor requires 'explicity' styling by providing links to CSS files.
 gulp.task('build-epiceditor', function() {
     return gulp.src('bower_components/epiceditor/epiceditor/themes/**/*.css')
-        .pipe(gulp.dest(distdir + '/epiceditor'));
+        .pipe(gulp.dest(dist.epiceditor));
 });
 
 gulp.task('build-vendor-css', function() {
-    return gulp.src(adminPaths.css)
+    return gulp.src(bowerPaths.css)
         .pipe(concat('admin.css'))
-        .pipe(gulp.dest(distdir));
+        .pipe(gulp.dest(dist.static));
 });
 
 gulp.task('build-vendor-js', function() {
-    return gulp.src(adminPaths.js)
+    return gulp.src(bowerPaths.js)
         .pipe(concat('admin.js'))
-        .pipe(gulp.dest(distdir));
+        .pipe(gulp.dest(dist.static));
 });
